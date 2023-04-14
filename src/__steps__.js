8d5c428de5db1e0b1f83bd7aaf10b31a02eb267b
email <allentusar909@gmail.com>
2023-04-14 18:02:03 +0600

    first commit

---
+++ src/__steps__.js
@@ -0,0 +1,7 @@
+/**
+ * 1.authentication
+ * 2.authorization
+ * 3.sign in === log in
+ * 4.sign up === register
+ * 5.
+ * */ 
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/layout/Main";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Home from "./components/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
